67ab2fafbccc1a9c824ba7ed7764cf749ccc54e0
Solyana Tafesse <sg.taffe@gmail.com>
2018-10-20 18:55:02 -0400

    fixed footer to the bottom of the page

---
+++ PR1/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const modal = document.getElementById("myModal");
+
+//const modalDiv = document.getElementById
+const img = document.getElementsByClassName("img-fluid");
+const modalImg = document.getElementById("img01");
+
+let i;
+let imagesLength = img.length;
+
+for (i = 0; i < imagesLength; i++) {
+  img[i].onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    document.querySelector("body").style.overflow = "hidden";
+  };
+}
+
+const span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+  document.querySelector("body").style.overflow = "auto";
+};
+
+modal.ondblclick = function() {
+  modal.style.display = "none";
+  document.querySelector("body").style.overflow = "auto";
+};
